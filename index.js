c5279f9352461c451d315a540ff73e27623aaf63
Gokulgurumurthy <gokulg4449@gmail.com>
2025-07-20 19:48:57 +0530

    learn function

--- index.js
+++ index.js
@@ -276,19 +276,15 @@ function greeting2(time) {
 // greeting2(6);
 // greetings(6);
 
-
 // countdown timer
 // create a function to count down from 100 - 0;
 
-
-function timer(){
-    for(i=100;i>0;i-=2){
-        console.log(i);
-    }
+function timer() {
+  for (i = 100; i > 0; i -= 2) {
+    console.log(i);
+  }
 }
 // timer()
-
-
 
 // function even(){
 //     for(i=100;i>0;i--){
@@ -299,26 +295,24 @@ function timer(){
 // }
 // even();
 
-
-function greetings5(){
-    console.log("wishing you a very happy bday");
-    console.log("god bless you")
+function greetings5() {
+  console.log("wishing you a very happy bday");
+  console.log("god bless you");
 }
 
 // greetings5()
 
-function greet1(royal){
-    if (royal === 452){
-        console.log("royal enfield " + "=", " its an new himalayan" );
-    } else if(royal === 350){
-        console.log("royal enfield " + "=","its an Meteor or classic")
-    } else {
-        console.log("Not an royal enfield")
-    }
+function greet1(royal) {
+  if (royal === 452) {
+    console.log("royal enfield " + "=", " its an new himalayan");
+  } else if (royal === 350) {
+    console.log("royal enfield " + "=", "its an Meteor or classic");
+  } else {
+    console.log("Not an royal enfield");
+  }
 }
 
 // greet1(350)
-
 
 //arrays and objects
 
@@ -334,11 +328,9 @@ function greet1(royal){
 // }
 // console.log(arr,obj)
 
-// arr 
+// arr
 
 // let arr1 = [1, '2', true, undefined, null,  [1, 2, 3, [1, 2, 3, 4]]];
-
-
 
 // let obj = {
 //   dob: "16/03/2000",
@@ -356,14 +348,11 @@ function greet1(royal){
 
 // arr = [1, '2', true, undefined, null, obj, [1, 2, 3, [1, 2, 3, 4]]];
 
-
 // console.log(arr);
-
 
 // let obj = {
 //   key: "value",
 // }
-
 
 // let str = "";
 
@@ -371,27 +360,20 @@ function greet1(royal){
 
 // console.log(str);
 
-
-
-
 // let alpha = "hello world";
-
 
 // alpha[1] = "v"
 // console.log(alpha)
-
 
 // const str = "hello";
 // str[1] = "h"
 
 // const arr = ["hello", "world", 1,2,3]
 
-
 // arr[0] = "morning";
 
 // console.log(arr)
 // console.log(str)
-
 
 // let a = "hello"; // #116ghha892
 // let b = a; // #117ghha892
@@ -406,28 +388,94 @@ function greet1(royal){
 
 // let e = d;
 
-// e[0] = "201" 
+// e[0] = "201"
 // console.log(c, d)
 
+// function objMutation(str) {
+//   return str
+// }
 
-
-function objMutation(str) {
-  return str
-}
-
-
-
-const obj = {
-  name: 'Hari',
-  age:25
-}
-
-
-
+// const obj = {
+//   name: 'Hari',
+//   age:25
+// }
 
 // const fnOutput = objMutation("hello") // => obj
 
-console.log(objMutation("hello"))
+// console.log(objMutation("hello"))
 
+// console.log(typeof null)
 
+// function fullname(lastname){
+// const firstname = "gokul";
+// const fullname = firstname + "gurumurthy";
+// console.log(fullname)
+// console.log("my name is " + fullname)
+// console.log(`my name is gokul ${lastname}` )
+// return `I am ${fullname}`
+// }
 
+// // fullname("gurumurthy")
+
+// console.log(fullname("gurumurthy"))
+
+// function countup(){
+//   for(i=0;i<=10;i++);
+//     console.log(i);
+// }
+// countup()
+// function countUp() {
+//   for (i = 0; i <= 10; i++) {
+//     console.log(i);
+//   }
+// }
+// countUp();
+
+// var declare time = 0
+// for 1 => 10
+// even + 1;
+// {
+// var time ++
+// }
+// return time;
+
+// function timer(){
+//   let time = 0;
+//   for(i=1;i<=10;i++){
+//     if(i%2==0){
+//       console.log(i+1);
+//     return
+//     }
+//   }
+
+// }
+
+// console.log(timer())
+
+// function timer(){
+//   var time ;
+// for(i=0;i<=10;i++){
+//   console.log(i);
+//   time = i
+//   if(i%2==0){
+//   }
+//     console.log(i+1)
+// }
+// return time
+// }
+// console.log(timer());
+
+function gokul() {
+  var time;
+  for (i = 1; i < 10; i++) {
+    if (i % 2 != 0) {
+      time = i + 1;
+      console.log(time, 'a')
+    } else {
+      time = i;
+      console.log(time, 'b')
+    }
+  }
+  // return time;
+}
+console.log(gokul());
