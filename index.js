779d682e899b661cb7cc56bb78417f26efd79dd6
Gokulgurumurthy <gokulg4449@gmail.com>
2025-07-23 23:51:07 +0530

    even odd

--- index.js
+++ index.js
@@ -819,8 +819,10 @@
 
 
 
-function evenOdd(num){
-  if(num%2 === 0) return "even";
-  return "odd"
-}
-console.log(evenOdd(5));
+// function evenOdd(num){
+//   if(num%2 === 0) return "even";
+//   return "odd"
+// }
+// console.log(evenOdd(5));
+
+
